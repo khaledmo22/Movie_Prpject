6de8ea9fd4641c033a21a85a336b79596f71d816
shahdashraf19 <shahdashr19@gmail.com>
2025-05-14 01:43:14 +0300

    completing auth screen ui design+signup,login/out functionality+Building the home screen

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,14 +1,13 @@
-
 import express from "express";
-import { login, signup, logout } from "../controllers/auth.controller.js";
+import { authCheck, login, logout, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.post("/signup",signup);
-
+router.post("/signup", signup);
 router.post("/login", login);
-
 router.post("/logout", logout);
 
-export default router;
+router.get("/authCheck", protectRoute, authCheck);
 
+export default router;
--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,9 +1,10 @@
+import { useAuthStore } from "../../store/authUser";
 import HomeScreen from "./HomeScreen";
 import AuthScreen from "./AuthScreen";
 
 const HomePage = () => {
-  const user=false;
-  return <div>{user?<HomeScreen/>:<AuthScreen/>}</div>
+  const { user } = useAuthStore();
+  return <>{user?<HomeScreen/>:<AuthScreen/>}</>
         {/* if user found go to home else go to auth */}
 
   
